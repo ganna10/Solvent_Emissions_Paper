abb7474fddd0801e51a3304413ea43a801a4097b
ganna10 <janecoates10@gmail.com>
2015-05-19 22:41:43 +0200

    Creating repro

---
+++ Scientific_Writing_Course/Solvent_Paper_Outline.mm
@@ -0,0 +1,51 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1429946478061" ID="ID_451407338" MODIFIED="1429946486931" TEXT="Solvent Paper Outline">
+<node CREATED="1429955374932" FOLDED="true" ID="ID_765483528" MODIFIED="1429957007164" POSITION="right" TEXT="Introduction">
+<node CREATED="1429954496346" ID="ID_684457798" MODIFIED="1429956622637" TEXT="Increased emissions of anthropogenic VOC main cause of climate change (IPCC)"/>
+<node CREATED="1429954560436" ID="ID_205002406" MODIFIED="1429954589961" TEXT="Models are used to determine and predict future scenarios"/>
+<node CREATED="1429954590602" ID="ID_925216967" MODIFIED="1429954673901" TEXT="Model inputs are determined by emission inventories of region of study -&gt; uncertainties in EI"/>
+<node CREATED="1429954913678" ID="ID_1589310612" MODIFIED="1429954943962" TEXT="Further uncertainty as not all species included in an EI may be represented in the model -&gt; chemistry"/>
+<node CREATED="1429955111821" ID="ID_368704259" MODIFIED="1429955167013" TEXT="Anthropogenic VOC emissions are not constant in time -&gt; temporal changes in VOC emissions not captured by EI"/>
+<node CREATED="1429947540261" ID="ID_340380613" MODIFIED="1429956643122" TEXT="Objective: Would changing EI make a difference to model output?"/>
+</node>
+<node CREATED="1429955417920" FOLDED="true" ID="ID_1781702190" MODIFIED="1429962014200" POSITION="right" TEXT="Materials &amp; Methods">
+<node CREATED="1429946567828" ID="ID_437632417" MODIFIED="1429947002987" TEXT="Boxmodel idealised study with maximum O3 production in each speciation&apos;s run, run time of 7 days"/>
+<node CREATED="1429946702852" ID="ID_1229061795" MODIFIED="1429946765198" TEXT="Idealised urban area of 1000 km2, total NMVOC emissions of 1000 tons day^-1"/>
+<node CREATED="1429946766348" ID="ID_1535749162" MODIFIED="1429946801263" TEXT="Solvent sector conributes 43% =&gt; total NMVOC emissions from solvent sector was 430 tons day^-1"/>
+<node CREATED="1429946803388" ID="ID_1139488408" MODIFIED="1429946850028" TEXT="Solvent speciations were used to determine the model inputs and calculate the contribution to total solvent emissions"/>
+<node CREATED="1429946851620" ID="ID_1104951323" MODIFIED="1429946891083" TEXT="Using MCM chemistry -&gt; detailed chemistry with &gt;100 primary NMVOC "/>
+<node CREATED="1429946892868" ID="ID_1268315441" MODIFIED="1429946962872" TEXT="To determine which MCM species were emitted used 1st solvent speciations and second the Passant 2002 report detailing individual NMVOC contributions to solvent emissions"/>
+<node CREATED="1429946969021" ID="ID_944465843" MODIFIED="1429946994636" TEXT="NMVOC emissions held constant until noon of first day to simulate an emission plume"/>
+<node CREATED="1429949169714" HGAP="28" ID="ID_896424339" MODIFIED="1429949243609" TEXT="Table: Initial Speciation VOC assigned to categories" VSHIFT="-1"/>
+<node CREATED="1429947043061" ID="ID_584255437" MODIFIED="1429947119916" TEXT="Used reduced mechanisms that are typically used for regional/global modelling to determine whether changing the representation changes the impacts on [O3]"/>
+<node CREATED="1429947155021" ID="ID_1278787999" MODIFIED="1429949764343" TEXT="Reference Speciation is TNO, other speciations are either model specific (IPCC/EMEP) or country and/or time specific (DE94/GR95/GR05/UK98/UK08). Brief description"/>
+</node>
+<node CREATED="1429955427389" FOLDED="true" ID="ID_1671367290" MODIFIED="1429962023609" POSITION="right" TEXT="Results">
+<node CREATED="1429946511389" ID="ID_106022521" MODIFIED="1429946521594" TEXT="Figure: [O3] time series"/>
+<node CREATED="1429949116138" ID="ID_705374562" MODIFIED="1429949158504" TEXT="Ox production budgets in different speciations &amp; mechanisms allocated to original categories"/>
+<node CREATED="1429946522395" ID="ID_1926435651" MODIFIED="1429946541622" TEXT="Table: 2nd differences in [O3] 2nd day maxima"/>
+<node CREATED="1429947249685" ID="ID_1338669242" MODIFIED="1429947314065" TEXT="Model (IPCC &amp; EMEP) speciations give highest [O3] for all mechanisms. Reference and country speciations are generally lower."/>
+<node CREATED="1429950302213" ID="ID_547582380" MODIFIED="1429950352883" TEXT="RADM2 has large difference between IPCC &amp; EMEP with all others lower =&gt; effect of lumping into different mechanism species"/>
+<node CREATED="1429950243893" ID="ID_880683921" MODIFIED="1429950301481" TEXT="MOZART tends to squash the time series together with lower differences between specitiations -&gt; effect of lumping into the same species i.e. larger alkanes and other categories as BIGALK"/>
+<node CREATED="1429950353917" ID="ID_8957877" MODIFIED="1429950782900" TEXT="MCM =&gt; large max - min and relatively evenly spread out time series but EMEP clearly higher"/>
+</node>
+<node CREATED="1429956445492" FOLDED="true" ID="ID_1154764254" MODIFIED="1429962022856" POSITION="right" TEXT="Discussion">
+<node CREATED="1429951894998" ID="ID_1094255732" MODIFIED="1429954389830" TEXT="IPCC &amp; EMEP have much larger contributions from alkanes -&gt; alkanes have a large potential to produce Ox over-multi day runs. This large Ox from alkanes is also underestimated when using 1 day model runs or OPP factors that are calculated using 1 day only. All other speciations have very large contributions from oxygenated species which have a tendency to produce less Ox (shown in Ox production budget plots)">
+<icon BUILTIN="yes"/>
+</node>
+<node CREATED="1429951994010" ID="ID_112004030" MODIFIED="1429954398504" TEXT="Focus of whether alkanes are represented correctly in models vs speciations and which alkanes.">
+<icon BUILTIN="yes"/>
+</node>
+<node CREATED="1429949530083" ID="ID_414003247" MODIFIED="1429954890464" TEXT="Differences in O3 between mechanisms mirror results from mechanism comparison study: MOZART lower as aromatics and large alkanes produced less Ox than MCM, RADM2 aromatics less O3 most alkanes less than MCM but HC3 produces more Ox =&gt; for RADM2 speciations with larger emissions of smaller VOC (IPCC/EMEP) may compensate for lower Ox from aromatics by overestimation from these smaller alkanes">
+<icon BUILTIN="yes"/>
+</node>
+</node>
+<node CREATED="1429956448737" FOLDED="true" ID="ID_83346482" MODIFIED="1429963236014" POSITION="right" TEXT="Conclusions">
+<node CREATED="1429947363646" ID="ID_1324021347" MODIFIED="1429947387700" TEXT="The choice of speciation does influence the amount of [O3] produced"/>
+<node CREATED="1429947393318" ID="ID_1024064281" MODIFIED="1429947417180" TEXT="In a non-idealised study these differences may be less"/>
+<node CREATED="1429947418382" ID="ID_1613368981" MODIFIED="1429947458364" TEXT="Effects of transport and dilution may also reduce the amount of O3 produced in 3-D models"/>
+<node CREATED="1429947511702" ID="ID_1442899237" MODIFIED="1429947705475" TEXT="Uncertainty in EI due to uncertainties in measurements, reporting from countries, how the EI was determined (top-down or bottom-up or source apportionment models). Combine the approaches for best-possible EI -&gt;future"/>
+</node>
+</node>
+</map>
